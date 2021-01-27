9a0e3a27445455e8815636a74988080f8ebd2438
kryznomas <kryztop@gmail.com>
2021-01-27 17:48:00 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+$ (document).ready(function() {  
+    
+    $("#elemento1").click(function() {
+        $("#elemento1").hide(1000);
+
+    })
+    
+    $("#elemento2").click(function() {
+        $("#elemento2").hide(1000);
+
+    })
+
+    $("#elemento3").click(function() {
+        $("#elemento3").hide(1000);
+
+    })
+
+    $("#elemento4").click(function() {
+        $("#elemento4").hide(1000);
+
+    })
+
+    $("#elemento5").click(function() {
+        $("#elemento5").hide(1000);
+
+    })
+
+    $("#elemento6").click(function() {
+        $("#elemento6").hide(1000);
+
+    })
+
+    $("#elemento7").click(function() {
+        $("#elemento7").hide(1000);
+
+    })
+
+    $("#elemento8").click(function() {
+        $("#elemento8").hide(1000);
+
+    })
+
+    $("#elemento9").click(function() {
+        $("#elemento9").hide(1000);
+
+    })
+
+    $("#boton").on('click', function() {
+        $("img").show(1000);
+        
+    });
+
+});  
